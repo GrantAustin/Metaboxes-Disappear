9beb1f91055e1a9cc2c9bc77c7e7cf82c42b4675
Grant Austin <insertfunnyemailhere@gmail.com>
2015-02-09 02:55:09 -0500

    Let's try again

--- metaboxes-disappear.php
+++ metaboxes-disappear.php
@@ -3,7 +3,7 @@
  * Plugin Name: I Hate Metaboxes 
  * Plugin URI: http://GrantAustin.org
  * Description: This plugin adds some Facebook Open Graph tags to our single posts.
- * Version: 1.0.2
+ * Version: 1.0.3
  * Author: Grant Austin
  * Author URI: http://GrantAustin.org
  * License: GPL2
@@ -11,27 +11,23 @@
 
 
 function remove_my_post_metaboxes() {
-	function remove_my_post_metaboxes() {
-		remove_meta_box( 'authordiv','post','normal' ); // Author Metabox
-		remove_meta_box( 'commentstatusdiv','post','normal' ); // Comments Status Metabox
-		remove_meta_box( 'commentsdiv','post','normal' ); // Comments Metabox
-		remove_meta_box( 'postcustom','post','normal' ); // Custom Fields Metabox
-		remove_meta_box( 'postexcerpt','post','normal' ); // Excerpt Metabox
-		remove_meta_box( 'revisionsdiv','post','normal' ); // Revisions Metabox
-		remove_meta_box( 'slugdiv','post','normal' ); // Slug Metabox
-		remove_meta_box( 'trackbacksdiv','post','normal' ); // Trackback Metabox
-	}
+	remove_meta_box( 'authordiv','post','normal' ); // Author Metabox
+	remove_meta_box( 'commentstatusdiv','post','normal' ); // Comments Status Metabox
+	remove_meta_box( 'commentsdiv','post','normal' ); // Comments Metabox
+	remove_meta_box( 'postcustom','post','normal' ); // Custom Fields Metabox
+	remove_meta_box( 'postexcerpt','post','normal' ); // Excerpt Metabox
+	remove_meta_box( 'revisionsdiv','post','normal' ); // Revisions Metabox
+	remove_meta_box( 'slugdiv','post','normal' ); // Slug Metabox
+	remove_meta_box( 'trackbacksdiv','post','normal' ); // Trackback Metabox
 }
 add_action('admin_menu','remove_my_post_metaboxes');
 
 function remove_my_page_metaboxes() {
-	function remove_my_page_metaboxes() {
-		remove_meta_box( 'postcustom','page','normal' ); // Custom Fields Metabox
-		remove_meta_box( 'postexcerpt','page','normal' ); // Excerpt Metabox
-		remove_meta_box( 'commentstatusdiv','page','normal' ); // Comments Metabox
-		remove_meta_box( 'trackbacksdiv','page','normal' ); // Talkback Metabox
-		remove_meta_box( 'slugdiv','page','normal' ); // Slug Metabox
-		remove_meta_box( 'authordiv','page','normal' ); // Author Metabox
-	}
+	remove_meta_box( 'postcustom','page','normal' ); // Custom Fields Metabox
+	remove_meta_box( 'postexcerpt','page','normal' ); // Excerpt Metabox
+	remove_meta_box( 'commentstatusdiv','page','normal' ); // Comments Metabox
+	remove_meta_box( 'trackbacksdiv','page','normal' ); // Talkback Metabox
+	remove_meta_box( 'slugdiv','page','normal' ); // Slug Metabox
+	remove_meta_box( 'authordiv','page','normal' ); // Author Metabox
 }
 add_action('admin_menu','remove_my_page_metaboxes');
