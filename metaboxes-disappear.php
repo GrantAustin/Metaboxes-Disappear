ba40fe6412f89663e95940bb1475f475a1c26bde
Grant Austin <kneblega@jay.washjeff.edu>
2016-03-03 06:23:44 -0500

    Added files via upload
    Trying to add roles conditional

--- metaboxes-disappear.php
+++ metaboxes-disappear.php
@@ -9,30 +9,32 @@
  * License: GPL2
  */
 
+if( current_user_can('families') ) {
+	function remove_my_post_metaboxes() {
+		remove_meta_box( 'commentstatusdiv','post','normal' ); // Discussion Metabox
+		remove_meta_box( 'postcustom','post','normal' ); // Custom Fields Metabox
+		remove_meta_box( 'postexcerpt','post','normal' ); // Excerpt Metabox
+		remove_meta_box( 'wpseo_meta', 'post', 'normal' );
+	}
+	add_action('admin_menu','remove_my_post_metaboxes');
 
-function remove_my_post_metaboxes() {
-	remove_meta_box( 'commentstatusdiv','post','normal' ); // Discussion Metabox
-	remove_meta_box( 'postcustom','post','normal' ); // Custom Fields Metabox
-	remove_meta_box( 'postexcerpt','post','normal' ); // Excerpt Metabox
-}
-add_action('admin_menu','remove_my_post_metaboxes');
+	function remove_my_events_metaboxes() {
+		remove_meta_box( 'commentstatusdiv','events','normal' ); // Discussion Metabox
+		remove_meta_box( 'postcustom','events','normal' ); // Custom Fields Metabox
+		remove_meta_box( 'postexcerpt','events','normal' ); // Excerpt Metabox
+	}
+	add_action('admin_menu','remove_my_events_metaboxes');
 
-function remove_my_events_metaboxes() {
-	remove_meta_box( 'commentstatusdiv','events','normal' ); // Discussion Metabox
-	remove_meta_box( 'postcustom','events','normal' ); // Custom Fields Metabox
-	remove_meta_box( 'postexcerpt','events','normal' ); // Excerpt Metabox
+	function remove_dashboard_widgets(){
+		remove_meta_box('dashboard_right_now', 'dashboard', 'normal');   // Right Now
+		remove_meta_box('dashboard_activity', 'dashboard', 'normal');   // Activity
+		remove_meta_box('dashboard_recent_comments', 'dashboard', 'normal'); // Recent Comments
+		remove_meta_box('dashboard_incoming_links', 'dashboard', 'normal');  // Incoming Links
+		remove_meta_box('dashboard_plugins', 'dashboard', 'normal');   // Plugins
+		remove_meta_box('dashboard_quick_press', 'dashboard', 'side');  // Quick Press
+		remove_meta_box('dashboard_recent_drafts', 'dashboard', 'side');  // Recent Drafts
+		remove_meta_box('dashboard_primary', 'dashboard', 'side');   // WordPress blog
+		remove_meta_box('dashboard_secondary', 'dashboard', 'side');   // Other WordPress News
+	}
 }
-add_action('admin_menu','remove_my_events_metaboxes');
-
-function remove_dashboard_widgets(){
-    remove_meta_box('dashboard_right_now', 'dashboard', 'normal');   // Right Now
-    remove_meta_box('dashboard_activity', 'dashboard', 'normal');   // Activity
-    remove_meta_box('dashboard_recent_comments', 'dashboard', 'normal'); // Recent Comments
-    remove_meta_box('dashboard_incoming_links', 'dashboard', 'normal');  // Incoming Links
-    remove_meta_box('dashboard_plugins', 'dashboard', 'normal');   // Plugins
-    remove_meta_box('dashboard_quick_press', 'dashboard', 'side');  // Quick Press
-    remove_meta_box('dashboard_recent_drafts', 'dashboard', 'side');  // Recent Drafts
-    remove_meta_box('dashboard_primary', 'dashboard', 'side');   // WordPress blog
-    remove_meta_box('dashboard_secondary', 'dashboard', 'side');   // Other WordPress News
-}
-add_action('wp_dashboard_setup', 'remove_dashboard_widgets');
+	add_action('wp_dashboard_setup', 'remove_dashboard_widgets');
